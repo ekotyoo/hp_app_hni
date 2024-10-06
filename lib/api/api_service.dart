74ce4d50142f8ac563dfb6d10d8d9c07c257df3b
ekotyoo <ekeprasetyo@gmail.com>
2024-10-06 21:54:55 +0700

    implement dio api service

---
+++ lib/api/api_service.dart
@@ -0,0 +1,37 @@
+import 'package:dio/dio.dart';
+
+enum DioHttpMethod { post, get, put, delete }
+
+class ApiService {
+  late Dio _dio;
+
+  ApiService() {
+    _dio = Dio(
+      BaseOptions(
+        baseUrl: baseUrl,
+        contentType: Headers.formUrlEncodedContentType,
+      ),
+    );
+  }
+
+  String get baseUrl {
+    return 'https://hp-api.onrender.com/api';
+  }
+
+  Future<Response> request(
+    String path,
+    DioHttpMethod method, {
+    Map<String, dynamic>? param,
+  }) {
+    try {
+      return switch (method) {
+        DioHttpMethod.post => _dio.post(path, data: param),
+        DioHttpMethod.get => _dio.get(path, data: param),
+        DioHttpMethod.put => _dio.put(path, data: param),
+        DioHttpMethod.delete => _dio.delete(path, data: param)
+      };
+    } catch (e) {
+      throw Exception('Connection error');
+    }
+  }
+}
